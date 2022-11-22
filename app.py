0942c8a06f00f7d260fd06825b62af3fa2a8e082
Javier <jmontilva94@gmail.com>
2022-11-22 06:29:51 -0400

    Add random secret key to Flask config

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from socket import IP_DROP_MEMBERSHIP
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from models import db
@@ -8,6 +9,7 @@ def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SECRET_KEY'] = os.urandom(32)
     db.init_app(app)
 
     from blueprints.tasks_list import tasks_list_api
