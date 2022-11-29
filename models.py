1d2fbd1b01f24c3878596bc96116fb52fec64ca5
Javier <jmontilva94@gmail.com>
2022-11-28 20:36:03 -0400

    Add serialize method to Task model

--- models.py
+++ models.py
@@ -5,6 +5,15 @@ db = SQLAlchemy()
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    description = db.Column(db.String(500))
-    complete = db.Column(db.Boolean, default=False)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(500), default="", nullable=False)
+    complete = db.Column(db.Boolean, default=False, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "complete": self.complete
+        }
