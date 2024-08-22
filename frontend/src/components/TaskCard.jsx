1d2da44668ee6c937315907eea1029fcd1f4e7b7
Javier Montilva <jmontilva@dstillery.com>
2024-08-22 10:13:29 -0400

    Refactor TaskCard.jsx component

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,26 +1,32 @@
-import React from 'react'
-import TaskDeleteButton from './TaskDeleteButton'
-import TaskEditButton from './TaskEditButton'
-import TaskStatusBadge from './TaskStatusBadge'
-import TaskChangeStatusButton from './TaskChangeStatusButton'
+import React, { useEffect, useState } from "react";
+import TaskDelete from "./TaskDelete";
+import TaskCheckbox from "./TaskCheckbox";
 
-function TaskCard({task}) {
-  return (
-    // <div>{task.title}</div>
-    <div className="row box item-section">
-            <div className="col-3 task-title">{task.title}</div>
-            <div className="col-3">
-                <TaskStatusBadge taskComplete={task.complete} />
+function TaskCard({ task }) {
+    const [currentTask, setCurrentTask] = useState(task);
+
+    useEffect(() => {
+        setCurrentTask(task);
+    }, [task]);
+
+    const handleTaskChange = (updatedTask) => {
+        setCurrentTask(updatedTask);
+    };
+
+    return (
+        <div className="row box item-section">
+            <div className="col-2 task-title">
+                <TaskCheckbox
+                    task={currentTask}
+                    onTaskChange={handleTaskChange}
+                />
             </div>
-            <div className="col-6">
-                <div className="btn-group-lg">
-                    <TaskChangeStatusButton task={task}/>
-                    <TaskEditButton task={task} />
-                    <TaskDeleteButton task={task} />
-                </div>
+            <div className="col-7 task-title">{task.title}</div>
+            <div className="col-3">
+                <TaskDelete task={task} />
             </div>
         </div>
-  )
+    );
 }
 
-export default TaskCard
+export default TaskCard;
